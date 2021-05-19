807c058919838f6272a6f23dec5907d687a4cb8e
Johnnes Souza <jhonnesf5@gmail.com>
2021-05-18 23:50:32 -0300

    Removed unsused variable

--- src/Orders/index.tsx
+++ src/Orders/index.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import Footer from '../Footer';
 import { fetchProducts, saveOrder } from './api';
 import { checkIsSelected } from './helpers';
-import OrderLocation from './OrderLocation';
+// import OrderLocation from './OrderLocation';
 import OrderSummary from './OrderSummary';
 import ProductsList from './ProductsList'
 import StepHeaders from './StepHeaders'
